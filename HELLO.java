994c30482e52f8408212b7df8bfd5ea87b82cfc2
icecor <a1456789sun@gmail.com>
2021-11-09 16:35:10 +0800

    This is my first commit

---
+++ HELLO.java
@@ -0,0 +1,11 @@
+public class HELLO {
+    public static void main(String[] args) {
+        int a = 175;
+        int b = 72;
+        System.out.println(b/((a/100)^2));
+
+
+
+
+    }
+}
---
+++ VARIABLE.java
@@ -0,0 +1,10 @@
+
+
+public class VARIABLE {
+    public static void main(String[] args) {
+        String x ="icecor";
+        System.out.println("hello "+ x);
+
+
+    }
+}
