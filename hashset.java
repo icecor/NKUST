1c678d9da070b3ae30ddcf1bc50349d9fb4dd23a
icecor <93572172+icecor@users.noreply.github.com>
2021-12-07 16:03:45 +0800

    Add files via upload

--- hashset.java
+++ hashset.java
@@ -1,23 +1,29 @@
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Scanner;
+import java.util.List;
+import java.util.Set;
 
 public class hashset {
     public static void main(String[] args) {
+        int[] num = {0,0,1,1,1,2,2,3,3,4};
+        List<Integer> numList = new ArrayList<Integer>();
+        Set<String> numSet = new HashSet<String>();
+        List<String> numList2 = new ArrayList<String>();
+        for (int i : num)
+        {
+            numList.add(i);
+            numSet.add(Integer.toString(i));
+        }
+        for (String i : numSet)
+            numList2.add(i);
 
-        //Scanner sc = new Scanner(System.in);
-        //Byte a =sc.nextByte();
-        HashSet<Integer> x =new HashSet<Integer>();
-        x.add(1);
-        x.add(1);
-        x.add(2);
+        int m =numList.size()-numSet.size();
 
+        for (int x = 0 ; x<m;x++)
+            numList2.add("_");
+        System.out.println(numList);
 
-
-        System.out.println("("+x+")");
-        System.out.println("有"+x.size()+"個整數");
-
-
-
+        System.out.println(numSet.size()+"個");
+        System.out.println(numList2);
     }
-
 }
